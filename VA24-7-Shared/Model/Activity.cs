3e189cd358839f086d53a37cd7100f3bae10ec3f
Hari Krishna <Harik@touchcoresystems.com>
2020-09-07 00:27:23 +0530

    Added azure function project to process the messages and insert into cosmos DB.

--- VA24-7-Shared/Model/Activity.cs
+++ VA24-7-Shared/Model/Activity.cs
@@ -2,11 +2,17 @@
 
 namespace VA24_7_Shared.Model
 {
-    public class Activity
+    public class Activity : IoT
     {
         public bool IsRunning { get; set; } = false;
         public bool IsSleeping { get; set; } = false;
         public int Pulserate { get; set; } = 50;
         public PulseStatus PulseStatus { get; set; } = PulseStatus.Normal;
+    }
+
+    public class IoT
+    {
+        public string DeviceId { get; set; }
+        public dynamic SystemProperties { get; set; }
     }
 }
